4df98040331248e3c8f941e9c7f6576f62986ca1
sirine <sirine@example.com>
2024-01-11 14:17:51 +0100

    logo

--- TALKS/djangochat/settings.py
+++ TALKS/djangochat/settings.py
@@ -134,9 +134,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+if DEBUG : 
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+else :
+    STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
